26718cb14ad78cc2e5504e651aa3ac6fd4c73cd0
LevivanderHeiden <42801613+LevivanderHeiden@users.noreply.github.com>
2018-09-18 12:17:53 +0200

    opdrachten lesdag 3

---
+++ assets/js/app.js
@@ -0,0 +1,32 @@
+//opdracht 1
+
+let number = 9;
+if (number % 2 == 0) {
+    console.log(`${number} is een even getal`);
+}
+else {
+    console.log(number + ' is een oneven getal');
+}
+
+
+//opdracht 2
+
+var str = 'Programming is not so cool';
+console.log(str);
+console.log(str.replace('not', ''));
+
+
+//opdracht 3
+const a = 1400
+const b = 'Ik woon in Naboo'
+console.log (a==b);
+//je vergelijkt een waarde met een string, dat is onzin.
+
+
+
+
+
+
+
+
+
